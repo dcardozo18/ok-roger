be8ee494bc91c6063eae62fb859346fff7d31e5e
Your Name <dcardozo18@gmail.com>
2025-10-17 23:22:44 +0000

    add the header like the rest of the pages

--- src/app/group-events/page.tsx
+++ src/app/group-events/page.tsx
@@ -2,12 +2,16 @@
 "use client";
 
 import { CalendarView } from "@/components/calendar/CalendarView";
+import { Header } from "@/components/dashboard/Header";
 import { groupEvents } from "@/lib/data";
 
 export default function GroupEventsPage() {
   return (
-    <div className="flex flex-col flex-1 h-screen bg-background">
-      <CalendarView initialEvents={groupEvents} />
+    <div className="flex flex-col bg-muted/40 flex-1">
+      <Header />
+      <main className="flex-1 flex flex-col">
+        <CalendarView initialEvents={groupEvents} />
+      </main>
     </div>
   );
 }
